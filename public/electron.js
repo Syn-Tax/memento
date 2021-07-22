2d53d5583677368653dbb1bd0a04d3e0d9950ecb
Oscar Morris <twocap06@gmail.com>
2021-07-22 13:25:10 +0100

    fixed build

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,6 @@
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, nativeTheme, session} = require('electron')
+const {app, BrowserWindow, nativeTheme} = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev');
 
