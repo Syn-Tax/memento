12fedac5b287867c59cfe887c3aa4154074f9cbb
Oscar Morris <twocap06@gmail.com>
2021-08-02 17:01:12 +0100

    changed create folder menu

---
+++ Changelog.md
@@ -0,0 +1,14 @@
+# v1.1.0-dev
+
+Added Features:
+
+- Creation of multiple choice creation
+
+# v1.0.0
+
+Added Features:
+
+- Material design User Interface
+- Allows for creation of custom folders and lists
+- Create your own lists of questions to memorise anything you want.
+
--- src/Components/CreateListFab.js
+++ src/Components/CreateListFab.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Fab, Menu, MenuItem } from '@material-ui/core';
+import { Fab, Menu, MenuItem, Dialog, DialogContent, DialogTitle, DialogActions, Button, TextField } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import { checkFolder, createFolder } from '../Utils/CreateFolder';
 
 function CreateListFab(props) {
     const [anchorEl, setAnchorEl] = React.useState(null)
+    const [folderDialog, setFolderDialog] = React.useState(false)
+    const [folderName, setFolderName] = React.useState("")
+    const [isInvalid, setIsInvalid] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState("")
+
     const history = useHistory()
 
     const OpenMenu = (event) => {
@@ -13,8 +19,7 @@ function CreateListFab(props) {
 
     const handleFolderItemClick = (event) => {
         setAnchorEl(null)
-        console.log(`/create-folder/${props.path}`)
-        history.push(`/create-folder/${props.path}`)
+        setFolderDialog(true)
     }
 
     const handleListItemClick = (event) => {
@@ -27,21 +32,59 @@ function CreateListFab(props) {
         setAnchorEl(null);
     }
 
+    const handleChange = (event) => {
+        let error = checkFolder(event.target.value, props.path.split("-"), props.gridItems)
+        if (error) {
+            setErrorMessage(error)
+            setIsInvalid(true)
+        } else {
+            setErrorMessage("")
+            setIsInvalid(false)
+        }
+
+        setFolderName(event.target.value)
+    }
+
+    const cancelCreateFolder = () => {
+        setFolderDialog(false)
+        setFolderName("")
+        setIsInvalid(false)
+        setErrorMessage("")
+    }
+
+    const submitCreateFolder = () => {
+        if (!isInvalid) {
+            console.log(folderName, props.path.split("-"))
+            createFolder(folderName, props.path.split("-"))
+            setFolderDialog(false)
+        }
+    }
+
     return (
         <div>
-            <Fab variant="extended" style={{ backgroundColor: "white" }} onClick={OpenMenu}>
-                <AddIcon style={{ opacity: 0.7 }} />
-                Create
-            </Fab>
-            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={CloseMenu}>
-                <MenuItem key="FOLDER" style={{ width: 150, fontSize: 14 }} onClick={handleFolderItemClick}>
-                    FOLDER
-                </MenuItem>
-                <MenuItem key="LIST" style={{ width: 150, fontSize: 14 }} onClick={handleListItemClick}>
-                    LIST
-                </MenuItem>
-            </Menu>
+          <Fab variant="extended" style={{ backgroundColor: "white" }} onClick={OpenMenu}>
+            <AddIcon style={{ opacity: 0.7 }} />
+            Create
+          </Fab>
+          <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={CloseMenu}>
+            <MenuItem key="FOLDER" style={{ width: 150, fontSize: 14 }} onClick={handleFolderItemClick}>
+              FOLDER
+            </MenuItem>
+            <MenuItem key="LIST" style={{ width: 150, fontSize: 14 }} onClick={handleListItemClick}>
+              LIST
+            </MenuItem>
+          </Menu>
 
+          <Dialog open={folderDialog} >
+            <DialogTitle>Create Folder</DialogTitle>
+            <DialogContent>
+              <TextField onChange={handleChange} variant="outlined" label={isInvalid ? "Error" : "Enter Folder Name"} error={isInvalid} helperText={isInvalid ? errorMessage : ""} fullwidth />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={cancelCreateFolder}>Cancel</Button>
+              <Button onClick={submitCreateFolder}>Submit</Button>
+            </DialogActions>
+          </Dialog>
         </div>
     );
 }
