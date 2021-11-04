82d67aa826e6088e1699f5d5bb26d90feb075047
Oscar Morris <twocap06@gmail.com>
2021-11-04 11:51:38 +0000

    finished refactoring

--- src/Views/CreateList.js
+++ src/Views/CreateList.js
@@ -9,15 +9,21 @@ import SaveButton from '../Components/SaveButton';
 import CreateQuestion from '../Components/CreateQuestion';
 import CreateQuestionFab from '../Components/CreateQuestionFab';
 
+/** 
+* @function CreateList - page that allows the user to create and edit lists
+* @return {JSX} - the JSX for the component
+*/
 function CreateList(props) {
+    // initialise all of the states/hooks
     const { pathStr } = useParams()
     const { search } = useLocation()
-    const [ questions, setQuestions ] = React.useState([])
-    const [ loadedQuestions, setLoadedQuestions ] = React.useState(false)
-    const [ invalidName, setInvalidName ] = React.useState(false)
-    const [ name, setName ] = React.useState("")
-    const [ errorMessage, setErrorMessage ] = React.useState("")
+    const [questions, setQuestions] = React.useState([])
+    const [loadedQuestions, setLoadedQuestions] = React.useState(false)
+    const [invalidName, setInvalidName] = React.useState(false)
+    const [name, setName] = React.useState("")
+    const [errorMessage, setErrorMessage] = React.useState("")
 
+    // load the questions and set other states 
     let queryStr = queryString.parse(search)
 
     if (queryStr["edit"] === "true") {
@@ -31,82 +37,82 @@ function CreateList(props) {
         if (pathStr === "Home") {
             qs = loadList(queryStr["list"])
         } else {
-            qs = loadList(pathStr+queryStr["list"])
+            qs = loadList(pathStr + queryStr["list"])
         }
         setQuestions(qs)
-        setName(queryStr["list"].substring(0, queryStr["list"].length-5))
+        setName(queryStr["list"].substring(0, queryStr["list"].length - 5))
         setLoadedQuestions(true)
     }
 
-    const onSave = (event) => {
+    const onSave = (event) => { // function that handles when the list is saved
         event.preventDefault()
         saveList(questions, name, pathStr)
     }
 
-    const nameChange = (event) => {
-        let error = checkFolder(event.target.value, pathStr.split("-"), props.gridItems)
-        if (error) {
+    const nameChange = (event) => { // function that handles when the name of the list is changed
+        let error = checkFolder(event.target.value, pathStr.split("-"), props.gridItems) // check if the name is valid
+
+        if (error) { // if there is an error show the error to the user
             setErrorMessage(error)
             setInvalidName(true)
         } else {
             setErrorMessage("")
             setInvalidName(false)
         }
-        setName(event.target.value)
+        setName(event.target.value) // otherwise set the same state to the new value
     }
 
-    const newItem = () =>{
-        let qs = questions
-        qs.push({"TITLE": "", "TYPE": "text", "ANSWERS": [""], "CORRECT": 0})
-        setQuestions([...qs])
-        console.log(questions)
+    const newItem = () => { // function that handles when a new question is added
+        let qs = questions // copy the questions to a new variable
+        qs.push({ "TITLE": "", "TYPE": "text", "ANSWERS": [""], "CORRECT": 0 }) // add the new question to it
+        setQuestions([...qs]) // set the questions as a new variable since react won't update the page when references are used
     }
 
-    const addAnswer = (index) => {
+    const addAnswer = (index) => { // function that handles when a new answer is added (very similar to the newItem function)
         let qs = questions
         qs[index]["ANSWERS"].push("")
         setQuestions([...qs])
     }
 
-    const removeAnswer = (index) => {
+    const removeAnswer = (index) => { // function that handles when an answer is removed
         let qs = questions
-        if (qs[index]["ANSWERS"].length > 1) {
+        if (qs[index]["ANSWERS"].length > 1) { // only remove an answer when there is more than one so that there must always be one answer
             qs[index]["ANSWERS"].pop()
         }
         setQuestions([...qs])
     }
-    
-    const deleteQuestion = (index) => {
+
+    const deleteQuestion = (index) => { // function that handles deleting a question
         let qs = questions
         qs.splice(index, 1)
         setQuestions([...qs])
     }
 
-    const titleChange = (index, event) => {
+    const titleChange = (index, event) => { // function that handles when the title of a question is changed (i.e. the question itself)
         let qs = questions
         qs[index]["TITLE"] = event.target.value
         setQuestions([...qs])
     }
-    
-    const answerChange = (questionIndex, answerIndex, event) => {
+
+    const answerChange = (questionIndex, answerIndex, event) => { // function that handles when an answer is changed
         let qs = questions
         qs[questionIndex]["ANSWERS"][answerIndex] = event.target.value
         setQuestions([...qs])
     }
 
-    const setCorrect = (questionIndex, correctIndex) => {
+    const setCorrect = (questionIndex, correctIndex) => { // function that sets the correct index of a multiple choice question
         let qs = questions
         qs[questionIndex]["CORRECT"] = correctIndex
         setQuestions([...qs])
     }
 
-    const setType = (questionIndex, type) => {
+    const setType = (questionIndex, type) => { // function that sets the type of the question
         let qs = questions
         qs[questionIndex]["TYPE"] = type
         setQuestions([...qs])
     }
 
-    const setImage = (questionIndex, id, name) => {
+    const setImage = (questionIndex, id, name) => { // function that sets the image associated with the question
         console.log(name, id)
         let qs = questions
         qs[questionIndex]["IMAGE_ID"] = id
@@ -116,7 +122,7 @@ function CreateList(props) {
 
     return (
         <div>
-            <Link to={pathStr==="Home" ? "/" : `/folder/${pathStr}`}><BackButton /></Link>
+            <Link to={pathStr === "Home" ? "/" : `/folder/${pathStr}`}><BackButton /></Link>
             <form onSubmit={onSave} style={{ paddingTop: "5%" }}>
                 <SaveButton />
                 <TextField onChange={nameChange} value={name} variant="outlined" label={invalidName ? "Error" : "Name"} error={invalidName} helperText={invalidName ? errorMessage : ""} style={{ width: "25%" }} />
--- src/Views/Folder.js
+++ src/Views/Folder.js
@@ -1,89 +1,90 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import NavbarToggle from '../Components/NavbarToggle'
 import CreateListFab from '../Components/CreateListFab';
 import SortMenu from '../Components/SortMenu';
 import ListGrid from '../Components/ListGrid';
 import BackButton from '../Components/BackButton';
-
 import { Grid } from '@material-ui/core';
 
-let count = 0
-
+/** 
+* @function getPath - function for traversing through the directory tree recursively and finding the parent object of an item
+* @param {Array} path - the path to the object from the current folder
+* @param {Array} lists - the list of items in the current folder
+* @param {String} parent - the path to the parent object of the current folder
+* @return {Array, String} - an array of the items in the current folder followed by the path to the parent object
+*/
 function getPath(path, lists, parent) {
 
-    if (path.length === 0) {
-        if (lists) {
-            return {lists, parent}
-        } else {
-            let lst = []
-            return {lst, parent}
-        }
+  if (path.length === 0) {
+    if (lists) {
+      return { lists, parent }
+    } else {
+      let lst = []
+      return { lst, parent }
     }
+  }
 
-    for (let i=0; i<lists.length; i++) {
-        let item = lists[i]
-        if (!item["PATH"]) {
-            continue
-        }
-        let pth = item["PATH"].split("-")
-        if (pth[pth.length-1] === path[0]) {
-
-            path.shift()
-            pth.pop()
-
-            return getPath(path, item["CONTAINS"], pth.join("-"))
-        } 
+  for (let i = 0; i < lists.length; i++) {
+    let item = lists[i]
+    if (!item["PATH"]) {
+      continue
     }
+    let pth = item["PATH"].split("-")
+    if (pth[pth.length - 1] === path[0]) {
+
+      path.shift()
+      pth.pop()
+
+      return getPath(path, item["CONTAINS"], pth.join("-"))
+    }
+  }
 }
 
+/** 
+* @function Folder - page that allows the user to view and traverse nested folders
+* @return {JSX} - the JSX for the component
+*/
 function Folder(props) {
-    const [sortMethod, setSortMethod] = React.useState("TYPE")
-    const { pathStr } = useParams()
-    const path = pathStr.split("-")
+  const [sortMethod, setSortMethod] = React.useState("TYPE")
+  const { pathStr } = useParams()
+  const path = pathStr.split("-")
 
-    let parentPath
+  // get the path to the parent object
+  let parentPath
 
-    if (path.length === 1) {
-        parentPath = "/"
-    } else {
-        let parentPth = [...path]
-        parentPth.pop()
-        parentPath = parentPth.join('-')
-        parentPath = `/folder/${parentPath}`
-    }
+  if (path.length === 1) {
+    parentPath = "/"
+  } else {
+    let parentPth = [...path]
+    parentPth.pop()
+    parentPath = parentPth.join('-')
+    parentPath = `/folder/${parentPath}`
+  }
 
-    let pth = getPath(path, props.gridItems, parentPath)
+  let pth = getPath(path, props.gridItems, parentPath)
 
-    console.log("PARENT PATH: ", parentPath)
+  let { lists, parent } = pth
 
-    let {lists, parent} = pth
+  return (
+    <div>
+      <Link to={parentPath}>
+        <BackButton />
+      </Link>
 
-    return (
-        <div>
-          { /*
-            <div style={{ position: "static" }}>
-                <NavbarToggle lists={props.gridItems} />
-            </div> */ }
+      <Grid container style={{ top: "10%", position: "absolute" }}>
+        <Grid item xs={3}>
+          <CreateListFab path={pathStr} gridItems={props.gridItems} />
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={3} style={{ display: 'flex', flexDirection: 'row' }} >
+          <SortMenu changeState={(method) => setSortMethod(method)} />
+        </Grid>
+      </Grid>
 
-            <Link to={parentPath}>
-                <BackButton />
-            </Link>
+      <ListGrid path={pathStr} parent={parentPath} items={lists} sortMethod={sortMethod} top={18} />
 
-            <Grid container style={{ top: "10%", position: "absolute" }}>
-                <Grid item xs={3}>
-                  <CreateListFab path={pathStr} gridItems={props.gridItems} />
-                </Grid>
-                <Grid item xs={6}></Grid>
-                <Grid item xs={3} style={{ display: 'flex', flexDirection: 'row' }} >
-                    <SortMenu changeState={(method) => setSortMethod(method)} />
-                </Grid>
-            </Grid>
-
-            <ListGrid path={pathStr} parent={parentPath} items={lists} sortMethod={sortMethod} top={18} />
-
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Folder;
